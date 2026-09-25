36fda986851665dfcf6244e2ca784d911dfb8f56
Denis Sorokin <r403prod@gmail.com>
2026-09-25 12:06:38 +0500

    feat: global config

---
+++ include/agent/config.h
@@ -0,0 +1,15 @@
+//
+// Created by user on 22.09.2026.
+//
+
+#ifndef INTEK_TESTOVOE_CONFIG_H
+#define INTEK_TESTOVOE_CONFIG_H
+#include <chrono>
+
+
+constexpr auto collect_interval = std::chrono::seconds(5);
+constexpr auto send_interval = std::chrono::seconds(30);
+constexpr int collect_limit = 10;
+
+
+#endif //INTEK_TESTOVOE_CONFIG_H
